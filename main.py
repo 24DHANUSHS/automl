4b4146d61dfde3be6cd5eb5b7a666bb6e8cb1d87
barathkumarg <yibarat1@gmail.com>
2021-12-09 10:28:52 +0530

    first coomit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,flash, render_template, request, redirect, url_for, session,Response,flash,jsonify,json
 import os
+import statistics
 import pandas as pd
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
@@ -26,6 +27,7 @@ def login():
 @app.route('/home',methods=['POST','GET'])
 def home():
     if 'file' in session:
+
         filename=session.get('file')
         project_name=session.get('name')
         type=session.get('type')
@@ -47,7 +49,87 @@ def home():
         sum1.append(sum(nan_len))
         data =df
         len1=row_len[0]+nan_len[0]
-        return render_template('index.html',cols=cols,cols_type=cols_type,row_len=row_len,nan_len=nan_len,len=len(cols),sum1=sum1,data=data,len1=len1,project_name=project_name,type=type)
+
+        #central tendency
+        #mean-median-mode
+        ct=[]
+        c=[]
+        for i in cols:
+            if df[i].dtype=='object':
+                c.append('Str')
+            else:
+                c.append(round(df[i].mean(),2))
+        ct.append(c)
+        #median
+        c=[]
+        for i in cols:
+            if df[i].dtype == 'object':
+                c.append('Str')
+            else:
+                c.append(round(df[i].median(),2))
+        ct.append(c)
+
+        #mode
+        c = []
+        for i in cols:
+            if df[i].dtype == 'object':
+                c.append('Str')
+            else:
+                c.append(statistics.mode(df[i]))
+        ct.append(c)
+        print(float('nan'))
+
+        return render_template('index.html',cols=cols,cols_type=cols_type,row_len=row_len,nan_len=nan_len,len=len(cols),sum1=sum1,data=data,len1=len1,project_name=project_name,type=type,ct=ct,na=float('nan'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/replace_nan',methods=['POST','GET'])
+def replace_nan():
+    if 'file' in session:
+        type=request.args.get('type')
+        cols=request.args.get('cols')
+        # data change code
+
+        filename = session.get('file')
+        df = pd.read_csv(filename)
+
+        if type=='mean':
+            df[cols].fillna(value=round(df[cols].mean(),2),inplace=True)
+
+        elif type=='median':
+            df[cols].fillna(value=round(df[cols].median(),2),inplace=True)
+        elif type=='mode':
+            df[cols].fillna(value=statistics.mode(df[cols]), inplace=True)
+
+        df.to_csv('{0}'.format(filename),index=False)
+        flash("you are successfuly logged in")
+        return redirect(url_for('home'))
+
+@app.route('/drop_dummies',methods=['POST','GET'])
+def drop_dummies():
+    if 'file' in session:
+        type=request.args.get('type')
+        cols=request.args.get('cols')
+        filename = session.get('file')
+        df = pd.read_csv(filename)
+        if type=='drop':
+
+            df.drop([cols], axis=1,inplace=True)
+            df.to_csv('{0}'.format(filename), index=False)
+            flash("you are successfuly logged in")
+
+            return redirect(url_for('home'))
+        elif type=='dummies':
+            df = pd.get_dummies(df, columns=[cols],drop_first=True)
+            df.to_csv('{0}'.format(filename), index=False)
+            return redirect(url_for('home'))
+        elif type=='dummies1':
+            df = df[df[cols].notna()]
+            df.to_csv('{0}'.format(filename), index=False)
+            return redirect(url_for('home'))
+
+
 
 
 
